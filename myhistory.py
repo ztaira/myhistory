1e7be95d5b4518ee46b76d3250ec2d9157b9f907
ztaira14 <ztaira14@punahou.edu>
2016-02-18 15:16:29 -0500

    Polished User Experience
    Made both rhist() and shist() print by default and not return anything.
    Also fiddled with the dictionary processing to make it print correctly.

--- myhistory.py
+++ myhistory.py
@@ -2,9 +2,9 @@
 from __future__ import print_function
 import readline
 
-def rhist(p=False, r=False):
-    """gets command line history. prints if p=true. returns command history if r=true.
-    otherwise, doesn't print and doesn't return command history."""
+def rhist(p=True, r=False):
+    """fetches command line history. prints if p=true. returns command history if r=true.
+    otherwise, doesn't print and/or doesn't return command history."""
     history_list = []
     # create history_list
     for i in range(readline.get_current_history_length()):
@@ -21,19 +21,26 @@ def getline(number):
     """returns the line at said number"""
     return readline.get_history_item(number)
 
-def shist(search_string, p=False, r=False):
+def shist(search_string, p=True, r=False):
     """Searches through the command line history. prints if p=true. returns command history if r=true.
-    otherwise, doesn't print and doesn't return command history."""
+    otherwise, doesn't print and/or doesn't return command history."""
     # gets the full list
-    history_list = rhist(r=True)
+    history_list = rhist(r=True, p=False)
+    history_dict = {}
+    for i in range(len(history_list)):
+        history_dict[i+1] = history_list[i]
     # trims the list down
-    for item in reversed(history_list):
-        if search_string not in item:
-            history_list.remove(item)
+    history_list = []
+    for key in history_dict:
+        if search_string not in history_dict[key]:
+            history_list.append(key)
+    for key in history_list:
+        if key in history_dict:
+            del history_dict[key]
     # print if p==True
     if (p==True):
-        for i in range(len(history_list)):
-            print(i+1, ":", history_list[i])
+        for key in history_dict:
+            print(key, ":", history_dict[key])
     # return partial history list
     if (r==True):
         return history_list
