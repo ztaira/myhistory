0a76dc4a76042b6b9f2853b0f668618ca9b8335c
ztaira14 <ztaira14@punahou.edu>
2016-02-18 13:22:35 -0500

    Functionality
    Things work now. The shist() lines numbers are still off. I suppose I
    should use a dictionary for that, but it serves its purpose for now.
    I’ll work on it later when I have time.

--- myhistory.py
+++ myhistory.py
@@ -1,40 +1,39 @@
+#!/usr/bin/env python
 from __future__ import print_function
 import readline
 
-def rhist(p):
-    """Returns the command line history. Short for 'return history'. p is for print."""
+def rhist(p=False, r=False):
+    """gets command line history. prints if p=true. returns command history if r=true.
+    otherwise, doesn't print and doesn't return command history."""
     history_list = []
-
     # create history_list
     for i in range(readline.get_current_history_length()):
-        history_list.append[readline.get_history_item(i+1)]
-
+        history_list.append(readline.get_history_item(i+1))
     # print if p==True
-    if (p == True):
-        for i in range(history_list.len()):
-            print(history_list[i])
-
-    # return total history list
-    return history_list
+    if (p==True):
+        for i in range(len(history_list)):
+            print(i+1, ":", history_list[i])
+    # return total history list if p==True
+    if (r==True):
+        return history_list
 
 def getline(number):
     """returns the line at said number"""
     return readline.get_history_item(number)
 
-def shist(search_string, p=False):
-    """Searches through the command line history and returns valid lines. Short for 'search hist'"""
+def shist(search_string, p=False, r=False):
+    """Searches through the command line history. prints if p=true. returns command history if r=true.
+    otherwise, doesn't print and doesn't return command history."""
     # gets the full list
-    history_list = rhist()
-
+    history_list = rhist(r=True)
     # trims the list down
-    for item in history_list:
+    for item in reversed(history_list):
         if search_string not in item:
             history_list.remove(item)
-
     # print if p==True
     if (p==True):
-        for i in range(history_list.len()):
-            print(history_list[i])
-
+        for i in range(len(history_list)):
+            print(i+1, ":", history_list[i])
     # return partial history list
-    return in_lines
+    if (r==True):
+        return history_list
